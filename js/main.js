dde6dfb8443ab2c07efca36e6567d2128ee5e722
Artur Tovkach <p.s.ovdok@gmail.com>
2023-04-10 22:28:00 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,38 @@
+$(document).scroll(function(){
+    if($(document).width() < 1024)
+        return false;
+    
+    if($(document).scrollTop() > $('.full-page').height() / 2)
+        $('header').addClass('fixed');
+    else
+    $('header').removeClass('fixed');
+})
+
+$(".up-btn").on("click", function(){
+    $("html, body").animate({
+        scrollTop: 0
+    }, 'slow')
+})
+
+$("#show-menu").on("click", function(){
+    $("#hidden-menu").animate({
+        "right": 0
+    }, 500)
+})
+
+$("#hidden-menu .close").on("click", function(){
+    $("#hidden-menu").animate({
+        "right": "-300px"
+    }, 500)
+})
+
+$(document).ready(function(){
+    $("#slider").slick({
+        dots: false,
+        prevArrow: '<div class="arrow-prev"> <i class="fa-solid fa-arrow-left"></div>',
+        nextArrow: '<div class="arrow-next"> <i class="fa-solid fa-arrow-right"></div>',
+        infinite: true,
+        slidesToShow:2,
+        slidesToScroll:1
+    });
+})
